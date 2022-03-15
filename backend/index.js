3f9ac9d45e2308a6756c5b30663ab97702416a25
natnael-OQP <makiya112545@gmail.com>
2022-03-15 20:20:29 +0300

    add middleware

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+// import
+const dbConnection = require("./config/db");
+const userRoutes = require("./routes/users");
+// initialize
+dotenv.config();
+dbConnection();
 const app = express();
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+// api
+app.use("/api/users", userRoutes);
 
-const port = process.env.PORT || 8000;
-
-app.listen(port, () => {
-  console.log(`server is running =>  http:localhost: ${posr}`);
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`backend server => http://localhost:8000`);
 });

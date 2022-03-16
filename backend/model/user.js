6954e5ade829607d77dae1fec22377a74ee6d929
natnael-OQP <makiya112545@gmail.com>
2022-03-16 09:27:19 +0300

    rename usernames to username

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,46 +1,46 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema(
-  {
-    usernames: {
-      type: String,
-      require: true,
-      unique: true,
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            require: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            require: true,
+            unique: true,
+        },
+        profilePic: {
+            type: String,
+            default: '',
+        },
+        coverPicture: {
+            type: String,
+            default: '',
+        },
+        followers: {
+            type: Array,
+            default: [],
+        },
+        following: {
+            type: Array,
+            default: [],
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
     },
-    email: {
-      type: String,
-      require: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      require: true,
-      unique: true,
-    },
-    profilePic: {
-      type: String,
-      default: "",
-    },
-    coverPicture: {
-      type: String,
-      default: "",
-    },
-    followers: {
-      type: Array,
-      default: [],
-    },
-    following: {
-      type: Array,
-      default: [],
-    },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+    {
+        timestamps: true,
+    }
+)
 
-module.export = mongoose.module("users", userSchema);
+module.exports = mongoose.model('User', UserSchema)

5d627aa3ae9d0a47a244122599a04ba4c7a0cdb7
natnael-OQP <makiya112545@gmail.com>
2022-03-16 10:01:12 +0300

    login completed

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -17,6 +17,7 @@ const registerUser = asyncHandler(async (req, res) => {
     try {
         if (!username || !email || !password) {
             res.status(400).json({ message: 'fill required fields' })
+            return
         }
         // hash password
         const salt = await bcrypt.genSalt(10)
@@ -35,12 +36,28 @@ const registerUser = asyncHandler(async (req, res) => {
         const user = await User.create(createdUser)
         res.status(200).json(user)
     } catch (error) {
-        res.status(404).json(error)
+        res.status(500).json(error)
     }
 })
 // login
 const loginUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'homepage' })
+    try {
+        const { username, password } = req.body
+        if (!username || !password) {
+            res.status(401).json({ message: 'fill required fields ' })
+            return
+        }
+        // check user
+        const user = await User.findOne({ username })
+        !user && res.status(404).json({ message: 'User Not Found' })
+        // match
+        const match = await bcrypt.compare(password, user.password)
+        !match && res.status(404).json({ message: 'Wrong Credential' })
+        const { password: pass, ...other } = user._doc
+        res.status(200).json(other)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 module.exports = {

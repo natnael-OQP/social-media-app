c03cf651df98de251734bd83cc131138af5555e1
natnael-OQP <makiya112545@gmail.com>
2022-03-17 11:06:36 +0300

    delete like-and-deslike posts

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -3,10 +3,16 @@ const {
     getPost,
     createPost,
     updatePost,
+    deletePost,
+    likeAndDislikePost,
+    getSinglePost,
 } = require('../controller/postController')
 
 router.get('/', getPost)
+router.get('/:id', getSinglePost)
 router.post('/', createPost)
 router.put('/:id', updatePost)
+router.delete('/:id', deletePost)
+router.put('/:id/like', likeAndDislikePost)
 
 module.exports = router

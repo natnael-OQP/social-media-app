e714f7f03a799d1da0f75976663b96a4e095dfa6
natnael-OQP <makiya112545@gmail.com>
2022-03-17 11:06:56 +0300

    get single post

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -11,6 +11,17 @@ const getPost = asyncHandler(async (req, res) => {
     }
 })
 
+// get Single post
+const getSinglePost = asyncHandler(async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        !post && res.status(404).json({ message: 'Port Not Found' })
+        res.status(200).json(post)
+    } catch (error) {
+        res.status(403).json(error)
+    }
+})
+
 // create post
 const createPost = asyncHandler(async (req, res) => {
     const { userId, desc } = req.body
@@ -27,7 +38,22 @@ const createPost = asyncHandler(async (req, res) => {
 })
 
 // delete post
-const deletePost = asyncHandler(async (req, res) => {})
+const deletePost = asyncHandler(async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        !post && res.status(404).json({ message: 'Post Not Found' })
+        if (req.body.userId === post.userId) {
+            await Post.findByIdAndDelete(req.params.id)
+            res.status(200).json({ message: 'post Deleted successfully' })
+        } else {
+            return res
+                .status(401)
+                .json({ message: "you can't delete other users post" })
+        }
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+})
 
 // update post
 const updatePost = asyncHandler(async (req, res) => {
@@ -46,20 +72,45 @@ const updatePost = asyncHandler(async (req, res) => {
         } else {
             return res
                 .status(401)
-                .json({ message: "yoy can't update other users post" })
+                .json({ message: "you can't update other users post" })
         }
     } catch (error) {
         return res.status(500).json(error)
     }
 })
 
-// like post
-const likePost = asyncHandler(async (req, res) => {})
+// like and dislike post
+const likeAndDislikePost = asyncHandler(async (req, res) => {
+    try {
+        let post = await Post.findById(req.params.id)
+        !post && res.status(404).json({ message: 'Post Not Found' })
+        if (req.body.userId !== post.userId) {
+            if (!post.likes.includes(req.body.userId)) {
+                await post.updateOne({ $push: { likes: req.body.userId } })
+                return res
+                    .status(200)
+                    .json({ message: 'the post has been liked' })
+            } else {
+                await post.updateOne({ $pull: { likes: req.body.userId } })
+                return res
+                    .status(200)
+                    .json({ message: 'the post has been disliked' })
+            }
+        } else {
+            return res
+                .status(401)
+                .json({ message: "you can't follow yur post" })
+        }
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+})
 
 module.exports = {
     getPost,
     createPost,
     deletePost,
     updatePost,
-    likePost,
+    likeAndDislikePost,
+    getSinglePost,
 }

79bf626a7c540c3ebc3c8180146d019cb74b4d2d
natnael-OQP <makiya112545@gmail.com>
2022-03-18 09:24:00 +0300

    Login page created

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './login.css'
+
+export default function Login() {
+    const [input, setInput] = useState()
+    const changeHandler = (e) => {
+        setInput(() => ({}))
+    }
+
+    return (
+        <div className="login">
+            <div className="loginWrapper">
+                <div className="loginLeft">
+                    <h3 className="loginLogo">Piper-chat </h3>
+                    <span className="loginDesc">
+                        Connect with friends and the world around you on
+                        Piper-chat.
+                    </span>
+                </div>
+                <div className="loginRight">
+                    <div className="loginBox">
+                        <input
+                            placeholder="Email"
+                            className="loginInput"
+                            name="email"
+                            onChange={changeHandler}
+                        />
+                        <input
+                            placeholder="Password"
+                            className="loginInput"
+                            name="password"
+                            onChange={changeHandler}
+                        />
+                        <button className="loginButton">Log In</button>
+                        <span className="loginForgot">Forgot Password?</span>
+                        <button className="loginRegisterButton">
+                            Create a New Account
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}

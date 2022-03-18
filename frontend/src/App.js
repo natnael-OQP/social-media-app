8fdd7d2b5afac028f4afc2ac9f3fcf8f512df5b1
natnael-OQP <makiya112545@gmail.com>
2022-03-18 10:50:14 +0300

    home page login and register route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+import Profile from './pages/profile/Profile'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
+    const user = true
+
     return (
-        <div className="">
-            <Home />
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={user ? <Home /> : <Register />} />
+                <Route path="/login" element={user ? <Home /> : <Login />} />
+            </Routes>
+        </Router>
     )
 }
 

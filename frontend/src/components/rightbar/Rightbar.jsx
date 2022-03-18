c3af4cf71a648f9368b41bcb9639ef2195fb282c
natnael-OQP <makiya112545@gmail.com>
2022-03-18 08:12:43 +0300

    rightbar created

---
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,71 @@
+import './rightbar.css'
+import { Users } from '../../dummyData'
+import Online from '../online/Online'
+
+export default function Rightbar({ profile }) {
+    const HomeRightbar = () => {
+        return (
+            <>
+                <div className="birthdayContainer">
+                    <img className="birthdayImg" src="assets/gift.png" alt="" />
+                    <span className="birthdayText">
+                        <b>Pola Foster</b> and <b>3 other friends</b> have a
+                        birhday today.
+                    </span>
+                </div>
+                <img className="rightbarAd" src="assets/ad.png" alt="" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    {Users.map((u) => (
+                        <Online key={u.id} user={u} />
+                    ))}
+                </ul>
+            </>
+        )
+    }
+
+    const ProfileRightbar = () => {
+        return (
+            <>
+                <h4 className="rightbarTitle">User information</h4>
+                <div className="rightbarInfo">
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">City:</span>
+                        <span className="rightbarInfoValue">Addise abeba</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">From:</span>
+                        <span className="rightbarInfoValue">Ethiopia</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">Relationship:</span>
+                        <span className="rightbarInfoValue">Single</span>
+                    </div>
+                </div>
+                <h4 className="rightbarTitle">User friends</h4>
+                <div className="rightbarFollowings">
+                    {Users.map((user) => (
+                        <div className="rightbarFollowing" key={user.id}>
+                            <img
+                                src={user.profilePicture}
+                                alt=""
+                                className="rightbarFollowingImg"
+                            />
+                            <span className="rightbarFollowingName">
+                                {user.username}
+                            </span>
+                        </div>
+                    ))}
+                </div>
+            </>
+        )
+    }
+
+    return (
+        <div className="rightbar">
+            <div className="rightbarWrapper">
+                {profile ? <ProfileRightbar /> : <HomeRightbar />}
+            </div>
+        </div>
+    )
+}

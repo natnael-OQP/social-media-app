f72fa8008b8ddd2f2e115b50412e20750d586e75
natnael-OQP <makiya112545@gmail.com>
2022-03-19 10:25:47 +0300

    get user from api

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -1,16 +1,27 @@
 import './post.css'
 import { MoreVert } from '@material-ui/icons'
-import { Users } from '../../dummyData'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import instance from '../../lib/axios'
 
 export default function Post({ post }) {
-    const [like, setLike] = useState(post.like)
+    console.log(post)
+    const [like, setLike] = useState(post?.likes.length)
     const [isLiked, setIsLiked] = useState(false)
-
+    const [user, setUser] = useState()
     const likeHandler = () => {
         setLike(isLiked ? like - 1 : like + 1)
         setIsLiked(!isLiked)
     }
+
+    useEffect(() => {
+        const fetcher = async () => {
+            const { data } = await instance.get(`users/${post.userId}`)
+            setUser(data)
+        }
+        fetcher()
+    }, [post.userId])
+    console.log(user)
+
     return (
         <div className="post">
             <div className="postWrapper">
@@ -19,18 +30,16 @@ export default function Post({ post }) {
                         <img
                             className="postProfileImg"
                             src={
-                                Users.filter((u) => u.id === post?.userId)[0]
-                                    .profilePicture
+                                user.profilePic
+                                    ? user.profilePic
+                                    : 'http://localhost:3000/assets/person/noAvatar.png'
                             }
                             alt=""
                         />
-                        <span className="postUsername">
-                            {
-                                Users.filter((u) => u.id === post?.userId)[0]
-                                    .username
-                            }
+                        <span className="postUsername">{user.username}</span>
+                        <span className="postDate">
+                            {new Date(user.createdAt).toDateString()}
                         </span>
-                        <span className="postDate">{post.date}</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVert />
@@ -38,7 +47,7 @@ export default function Post({ post }) {
                 </div>
                 <div className="postCenter">
                     <span className="postText">{post?.desc}</span>
-                    <img className="postImg" src={post.photo} alt="" />
+                    <img className="postImg" src={post?.img} alt="" />
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">

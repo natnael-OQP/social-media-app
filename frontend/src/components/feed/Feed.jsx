1134099b3f66132d9d987836fbe69ca8461d2acd
natnael-OQP <makiya112545@gmail.com>
2022-03-19 10:21:13 +0300

    fetch user by userId

--- frontend/src/components/feed/Feed.jsx
+++ frontend/src/components/feed/Feed.jsx
@@ -1,14 +1,27 @@
 import Post from '../post/Post'
 import Share from '../share/Share'
 import './feed.css'
-import { Posts } from '../../dummyData'
+import { useEffect, useState } from 'react'
+import instance from '../../lib/axios'
+import Spinner from '../spinner/Spinner'
 
 export default function Feed() {
+    const [posts, setPosts] = useState()
+    useEffect(() => {
+        const fetcher = async () => {
+            const { data } = await instance.get(
+                'posts/timeline/62331709f253d8840ea9b78d'
+            )
+            setPosts(data)
+        }
+        fetcher()
+    }, [])
+    if (!posts) return <Spinner />
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share />
-                {Posts.map((post) => (
+                {posts?.map((post) => (
                     <Post key={post.id} post={post} />
                 ))}
             </div>

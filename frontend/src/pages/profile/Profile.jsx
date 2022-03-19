08b2ddd08b830199ecfc4b32ebe6930b8fbaa1d5
natnael-OQP <makiya112545@gmail.com>
2022-03-19 18:28:28 +0300

    pass username value

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -32,7 +32,7 @@ export default function Profile() {
                         </div>
                     </div>
                     <div className="profileRightBottom">
-                        <Feed />
+                        <Feed username="natnael-oqp" />
                         <Rightbar profile />
                     </div>
                 </div>

8fa76d37634a2a49fddbec37ddc299aef8070d66
natnael-OQP <makiya112545@gmail.com>
2022-03-20 12:31:42 +0300

    fill user info

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -2,16 +2,19 @@ import './rightbar.css'
 import { Users } from '../../dummyData'
 import Online from '../online/Online'
 
-export default function Rightbar({ profile }) {
+export default function Rightbar({ user }) {
     const HomeRightbar = () => {
         return (
             <>
                 <div className="birthdayContainer">
+                    {/*
+            
                     <img className="birthdayImg" src="assets/gift.png" alt="" />
                     <span className="birthdayText">
                         <b>Pola Foster</b> and <b>3 other friends</b> have a
                         birhday today.
                     </span>
+                    */}
                 </div>
                 <img className="rightbarAd" src="assets/ad.png" alt="" />
                 <h4 className="rightbarTitle">Online Friends</h4>
@@ -23,23 +26,24 @@ export default function Rightbar({ profile }) {
             </>
         )
     }
-
     const ProfileRightbar = () => {
         return (
             <>
                 <h4 className="rightbarTitle">User information</h4>
                 <div className="rightbarInfo">
                     <div className="rightbarInfoItem">
-                        <span className="rightbarInfoKey">City:</span>
-                        <span className="rightbarInfoValue">Addise abeba</span>
-                    </div>
-                    <div className="rightbarInfoItem">
                         <span className="rightbarInfoKey">From:</span>
                         <span className="rightbarInfoValue">Ethiopia</span>
                     </div>
                     <div className="rightbarInfoItem">
-                        <span className="rightbarInfoKey">Relationship:</span>
-                        <span className="rightbarInfoValue">Single</span>
+                        <span className="rightbarInfoKey">City:</span>
+                        <span className="rightbarInfoValue">{user?.city}</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">Education:</span>
+                        <span className="rightbarInfoValue">
+                            {user?.education}
+                        </span>
                     </div>
                 </div>
                 <h4 className="rightbarTitle">User friends</h4>
@@ -64,7 +68,7 @@ export default function Rightbar({ profile }) {
     return (
         <div className="rightbar">
             <div className="rightbarWrapper">
-                {profile ? <ProfileRightbar /> : <HomeRightbar />}
+                {user ? <ProfileRightbar /> : <HomeRightbar />}
             </div>
         </div>
     )

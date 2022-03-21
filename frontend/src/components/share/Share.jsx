8675ff9be61e34f1ce1635c1395f7e2f7ac2c4a1
natnael-OQP <makiya112545@gmail.com>
2022-03-21 14:35:59 +0300

    post upload completed

--- frontend/src/components/share/Share.jsx
+++ frontend/src/components/share/Share.jsx
@@ -1,7 +1,49 @@
-import './share.css'
+import { useContext, useRef, useState } from 'react'
 import { PermMedia, Label, Room, EmojiEmotions } from '@material-ui/icons'
+import './share.css'
+import { context } from '../../context/context.js'
+import instance from '../../lib/axios'
+import axios from 'axios'
+import Spinner from '../../components/spinner/Spinner'
 
 export default function Share({ user }) {
+    const { user: authUser } = useContext(context)
+    const [file, setFile] = useState()
+    const descRef = useRef()
+
+    const handelSubmit = async (e) => {
+        e.preventDefault()
+        const data = new FormData()
+        data.append('file', file)
+        data.append('upload_preset', 'uploads')
+
+        try {
+            if (file) {
+                const upload = await axios.post(
+                    `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_ID}/image/upload`,
+                    data
+                )
+                const { url } = upload.data
+                const newPost = {
+                    userId: authUser._id,
+                    desc: descRef.current.value,
+                    img: url,
+                }
+                await instance.post('posts', newPost)
+                window.location.reload()
+            } else {
+                const newPost = {
+                    userId: authUser._id,
+                    desc: descRef.current.value,
+                }
+                await instance.post('posts', newPost)
+                window.location.reload()
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className="share">
             <div className="shareWrapper">
@@ -15,21 +57,30 @@ export default function Share({ user }) {
                         alt="avatar"
                     />
                     <input
-                        placeholder="What's in your mind Safak?"
+                        type="text"
+                        ref={descRef}
+                        placeholder={`What's in your mind ${authUser?.username}?`}
                         className="shareInput"
                     />
                 </div>
                 <hr className="shareHr" />
-                <div className="shareBottom">
+                <form onSubmit={handelSubmit} className="shareBottom">
                     <div className="shareOptions">
                         <div className="shareOption">
-                            <PermMedia
-                                htmlColor="tomato"
-                                className="shareIcon"
-                            />
-                            <span className="shareOptionText">
+                            <label htmlFor="file" className="shareOptionText">
+                                <PermMedia
+                                    htmlColor="tomato"
+                                    className="shareIcon"
+                                />
                                 Photo or Video
-                            </span>
+                                <input
+                                    type="file"
+                                    id="file"
+                                    style={{ display: 'none' }}
+                                    accept=".png,.jpeg,.jpg"
+                                    onChange={(e) => setFile(e.target.files[0])}
+                                />
+                            </label>
                         </div>
                         <div className="shareOption">
                             <Label htmlColor="blue" className="shareIcon" />
@@ -47,8 +98,10 @@ export default function Share({ user }) {
                             <span className="shareOptionText">Feelings</span>
                         </div>
                     </div>
-                    <button className="shareButton">Share</button>
-                </div>
+                    <button type="submit" className="shareButton">
+                        Share
+                    </button>
+                </form>
             </div>
         </div>
     )

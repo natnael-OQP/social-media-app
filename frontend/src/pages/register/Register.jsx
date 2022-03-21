c4a1dd0fc43d816eff242ee9150b6619068af17a
natnael-OQP <makiya112545@gmail.com>
2022-03-21 09:22:25 +0300

    register user completed

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,8 +1,41 @@
 import './register.css'
 import { useNavigate } from 'react-router-dom'
+import { useRef, useState } from 'react'
+import instance from '../../lib/axios'
 
 export default function Register() {
+    const [input, setInput] = useState()
+    const passwordRef = useRef()
     const navigation = useNavigate()
+
+    const changeHandler = (e) => {
+        setInput((prev) => {
+            return {
+                ...prev,
+                [e.target.name]: e.target.value,
+            }
+        })
+    }
+
+    const handelSubmit = async (e) => {
+        e.preventDefault()
+        if (input.password !== input.confirmPassword) {
+            alert("Password dos't match!")
+        } else {
+            const user = {
+                email: input.email,
+                username: input.username,
+                password: input.password,
+            }
+            try {
+                await instance.post('auth/register', user)
+                navigation('/login')
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
+
     return (
         <div className="login">
             <div className="loginWrapper">
@@ -14,22 +47,50 @@ export default function Register() {
                     </span>
                 </div>
                 <div className="loginRight">
-                    <div className="loginBox">
-                        <input placeholder="Username" className="loginInput" />
-                        <input placeholder="Email" className="loginInput" />
-                        <input placeholder="Password" className="loginInput" />
+                    <form onSubmit={handelSubmit} className="loginBox">
                         <input
+                            name="username"
+                            type="text"
+                            placeholder="Username"
+                            className="loginInput"
+                            required={true}
+                            onChange={changeHandler}
+                        />
+                        <input
+                            name="email"
+                            type="email"
+                            placeholder="Email"
+                            className="loginInput"
+                            required={true}
+                            onChange={changeHandler}
+                        />
+                        <input
+                            name="password"
+                            type="password"
+                            placeholder="Password"
+                            className="loginInput"
+                            ref={passwordRef}
+                            required={true}
+                            onChange={changeHandler}
+                        />
+                        <input
+                            name="confirmPassword"
+                            type="password"
                             placeholder="Password Again"
                             className="loginInput"
+                            required={true}
+                            onChange={changeHandler}
                         />
-                        <button className="loginButton">Sign Up</button>
+                        <button type="submit" className="loginButton">
+                            Sign Up
+                        </button>
                         <button
                             onClick={() => navigation('/login')}
                             className="loginRegisterButton"
                         >
                             Log into Account
                         </button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>

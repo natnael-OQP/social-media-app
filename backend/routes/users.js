81e729c52d80a807f7227aef4ee294ed8a1d5fd4
natnael-OQP <makiya112545@gmail.com>
2022-03-22 22:13:25 +0300

    frindes/:id get frindes routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,10 +5,12 @@ const {
     getUser,
     followUser,
     unFollowUser,
+    getFiends,
 } = require('../controller/userController')
 
 // routes
 router.get('/:id', getUser)
+router.get('/friends/:id', getFiends)
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
 router.put('/follow/:id', followUser)

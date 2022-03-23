4c1c85ce28c78ae6ad05467f4c6fbfbb4e2b6fca
natnael-OQP <makiya112545@gmail.com>
2022-03-23 10:09:52 +0300

    completed

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -3,18 +3,21 @@ import Online from '../online/Online'
 import { useContext, useEffect, useState } from 'react'
 import instance from '../../lib/axios'
 import { context } from '../../context/context.js'
-import { Link, useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Add, Remove } from '@material-ui/icons'
 
 export default function Rightbar({ users }) {
+    const { user: authUser, dispatch } = useContext(context)
     const [friends, setFriends] = useState()
-    const [followed, setFollowed] = useState(users.follower)
-    const { user: authUser } = useContext(context)
+    const [followed, setFollowed] = useState(
+        authUser.following.includes(users?._id)
+    )
+
     useEffect(() => {
         const fetcher = async () => {
             try {
                 const { data } = await instance.get(
-                    `users/friends/${authUser._id}`
+                    `users/friends/${authUser?._id}`
                 )
                 setFriends(data)
             } catch (error) {
@@ -24,46 +27,35 @@ export default function Rightbar({ users }) {
         fetcher()
     }, [])
 
-    useEffect(() => {
-        setFollowed(authUser.following.includes(users?._id))
-    }, [users?._id, authUser])
-
     const handelClick = async () => {
         try {
             if (followed) {
-                await instance.put(`users/follow/${users._id}`, {
-                    userId: authUser._id,
-                })
-            } else {
                 await instance.put(`users/unfollow/${users._id}`, {
                     userId: authUser._id,
                 })
+                dispatch({ type: 'unFollow', payload: users?._id })
+            } else {
+                await instance.put(`users/follow/${users._id}`, {
+                    userId: authUser._id,
+                })
+                dispatch({ type: 'Follow', payload: users?._id })
             }
+            setFollowed(!followed)
         } catch (error) {
             console.log(error)
         }
-        setFollowed(!followed)
     }
-    console.log(followed)
+    console.log(authUser)
 
     const HomeRightbar = () => {
         return (
             <>
-                <div className="birthdayContainer">
-                    {/*
-            
-                    <img className="birthdayImg" src="assets/gift.png" alt="" />
-                    <span className="birthdayText">
-                        <b>Pola Foster</b> and <b>3 other friends</b> have a
-                        birhday today.
-                    </span>
-                    */}
-                </div>
+                <div className="birthdayContainer"></div>
                 <img className="rightbarAd" src="assets/ad.png" alt="" />
                 <h4 className="rightbarTitle">Online Friends</h4>
                 <ul className="rightbarFriendList">
-                    {friends?.map((u) => (
-                        <Online key={u._id} user={u} />
+                    {friends?.map((u, i) => (
+                        <Online key={u._id + i} user={u} />
                     ))}
                 </ul>
             </>
@@ -106,8 +98,8 @@ export default function Rightbar({ users }) {
                 {/* ---- user friends  ---- */}
                 <h4 className="rightbarTitle">User friends</h4>
                 <div className="rightbarFollowings">
-                    {friends?.map((user) => (
-                        <div className="rightbarFollowing" key={user?._id}>
+                    {friends?.map((user, i) => (
+                        <div className="rightbarFollowing" key={user?._id + i}>
                             <Link to={`/profile/${user?._id}`}>
                                 <img
                                     src={

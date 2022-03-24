25bcee042dd694a646d314f3285da69ff8fa8d3d
natnael-OQP <makiya112545@gmail.com>
2022-03-24 10:14:33 +0300

    Chat Online omponent created

---
+++ frontend/src/components/chatOnline/ChatOnline.jsx
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import './chatOnline.css'
+
+export default function ChatOnline({ onlineUsers, currentId, setCurrentChat }) {
+    const [friends, setFriends] = useState([])
+    const [onlineFriends, setOnlineFriends] = useState([])
+
+    useEffect(() => {
+        const getFriends = async () => {}
+        getFriends()
+    }, [])
+
+    useEffect(() => {}, [])
+
+    const handleClick = async (user) => {}
+
+    return (
+        <div className="chatOnline">
+            {onlineFriends.map((o) => (
+                <div
+                    className="chatOnlineFriend"
+                    onClick={() => handleClick(o)}
+                >
+                    <div className="chatOnlineImgContainer">
+                        <img
+                            className="chatOnlineImg"
+                            src={o?.profilePicture}
+                            alt=""
+                        />
+                        <div className="chatOnlineBadge"></div>
+                    </div>
+                    <span className="chatOnlineName">{o?.username}</span>
+                </div>
+            ))}
+        </div>
+    )
+}

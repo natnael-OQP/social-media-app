2a9ddd95fbe2828df36c6afad662b70ef81bda87
natnael-OQP <makiya112545@gmail.com>
2022-03-24 10:08:17 +0300

    conversation routes created

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,14 @@ const dbConnection = require('./config/db')
 const userRoutes = require('./routes/users')
 const authRoutes = require('./routes/auth')
 const postRoutes = require('./routes/post')
+const conversationRoutes = require('./routes/conversation')
+const messageRoutes = require('./routes/message')
+
 // initialize
 dotenv.config()
 dbConnection()
 const app = express()
+
 // middleware
 app.use(express.json())
 app.use(helmet())
@@ -22,6 +26,8 @@ app.use(cors())
 app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/posts', postRoutes)
+app.use('/api/conversation', conversationRoutes)
+app.use('/api/message', messageRoutes)
 
 app.listen(process.env.PORT || 8000, () => {
     console.log(`backend server => http://localhost:8000`)

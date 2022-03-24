694d9c4ad882058d5bf1e653dabe63b41ea87457
natnael-OQP <makiya112545@gmail.com>
2022-03-24 11:01:52 +0300

    get message by conversation Id

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -11,12 +11,13 @@ const createMessage = asyncHandler(async (req, res) => {
     }
 })
 
-// create
+// get
 const getMessage = asyncHandler(async (req, res) => {
-    
     try {
-        const message = await Message.create(req.body)
-        res.status(200).json(message)
+        const messages = await Message.find({
+            conversationId: req.params.id,
+        })
+        res.status(200).json(messages)
     } catch (error) {
         res.status(500).json(error)
     }

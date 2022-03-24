8006bf4b8bfcb3ee37bd5827be2708a59256ad4e
natnael-OQP <makiya112545@gmail.com>
2022-03-24 11:02:46 +0300

    add id params

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 const { createMessage, getMessage } = require('../controller/messageController')
 
 router.post('/', createMessage)
-router.get('/', getMessage)
+router.get('/:id', getMessage)
 
 module.exports = router

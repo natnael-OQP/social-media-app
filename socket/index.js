7672484c5b6a5a83985c2adecb1f2f49f2ffb894
natnael-OQP <makiya112545@gmail.com>
2022-03-25 22:27:46 +0300

    get userId from client

--- socket/index.js
+++ socket/index.js
@@ -1,9 +1,30 @@
 const io = require('socket.io')(8900, {
     cors: {
-        origin: 'https://piper-chats-api.herokuapp.com',
+        origin: 'http://localhost:3000',
     },
 })
 
+let users = []
+
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) &&
+        users.push({ userId, socketId })
+}
+
+const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId)
+}
+
 io.on('connection', (socket) => {
     console.log('a user connected')
+    // take user id and socket id from user
+    socket.on('addUser', (userId) => {
+        addUser(userId, socket.id)
+        io.emit('getUsers', users)
+    })
+    socket.on('disconnect', () => {
+        console.log('a user disconnected')
+        removeUser(socket.id)
+        io.emit('getUsers', users)
+    })
 })
